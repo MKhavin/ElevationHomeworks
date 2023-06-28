f4a4a7f4c2e7eed5ec203ae456401ef234b3a99b
MichaelK <havinmichael@gmail.com>
2023-06-28 22:42:54 +0300

    Complete cart extension of block jQuery

--- week-5/Lesson1/Exercise 4/main.js
+++ week-5/Lesson1/Exercise 4/main.js
@@ -1,13 +1,47 @@
+const cart = []
+
 function addItemToCart(item) {
     const itemInStock = $(item).data().instock
 
     if (itemInStock) {
-        $("#cart").append(`<div class=cart-item>${$(item).text()}</div>`)
+        const itemName = $(item).text()
+        let itemInCart = cart.find(value => value.name === itemName)
+
+        if (itemInCart !== undefined) {
+            itemInCart.quantity += 1
+        } else {
+            itemInCart = {
+                name: itemName,
+                quantity: 1
+            }
+
+            cart.push(itemInCart)
+        }
+        renderCart()
     }
 }
 
+function renderCart() {
+    const cartEl = $("#cart")
+    cartEl.empty()
+    cart.forEach(item => cartEl.append(`<div class=cart-item data-name='${item.name}'}>${item.name + (item.quantity > 1 ? ` x${item.quantity}` : "")}</div>`))
+}
+
 function itemClicked() {
-   addItemToCart(this)
+    addItemToCart(this)
+}
+
+function cartItemClicked() {
+    const item = $(this)
+    const itemName = item.data().name
+
+    let itemInCart = cart.findIndex(value => value.name === itemName)
+    if (itemInCart !== -1) {
+        cart.splice(itemInCart, 1)
+    }
+
+    item.remove()
 }
 
 $(".item").click(itemClicked)
+$("#cart").on("click", ".cart-item", cartItemClicked)
