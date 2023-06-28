1311ebf6e0caf4586c94584ab24b153f91a32086
MichaelK <havinmichael@gmail.com>
2023-06-28 14:20:11 +0300

    Complete all SpotChecks of block jQuery

--- week-5/Lesson1/main.js
+++ week-5/Lesson1/main.js
@@ -30,7 +30,93 @@ function setInputValue() {
   $("#my-input").val("Terabyte")
 }
 
+function logBarcodeData() {
+  const barcodeData = $("#barcode").data()
+
+  console.log(`The item with barcode ${barcodeData.barcode} will expire on ${barcodeData.expirationDate}`)
+}
+
+function setHoverEventListener() {
+  function elementHovered(event) {
+    const target = $(event.target)
+    target.addClass("blue-background-color")
+  }
+
+  function elementUnhovered(event) {
+    const target = $(event.target)
+    target.removeClass("blue-background-color")
+  }
+
+  const hoverElement = $("#hover")
+  hoverElement.css("width", "100px")
+  hoverElement.css("height", "100px")
+
+  hoverElement.on("mouseenter", elementHovered)
+  hoverElement.on("mouseleave", elementUnhovered)
+}
+
+function setClickEventListener() {
+  function elementClicked(event) {
+    alert($("#my-second-input").val())
+  }
+
+  $("#alert-button").click(elementClicked)
+}
+
+function setBoxHoverEventListener() {
+  $(".box").on("mouseenter", function () { $(this).css("background-color", "blue") })
+  $(".box").on("mouseleave", function () { $(this).css("background-color", "") })
+}
+
+function setFeedMeFunctionality() {
+  $(".feedme").on("click", function () {
+    let divCopy = `<div class=feedme>${$(this).text()}</div>`
+    $("body").append(divCopy)
+  })
+}
+
+function addElementsInLoop() {
+  const names = [
+    { first: "Alex", last: "Johnson" },
+    { first: "Byron", last: "Loveall" },
+    { first: "Cassandra", last: "Wuthers" },
+    { first: "Deandre", last: "Rahm" },
+    { first: "Ellena", last: "Freeman" }
+  ]
+
+  names.forEach(value => $("#spotcheck-10").append(`<p>${value.first} - ${value.last}</p>`))
+}
+
+function setPoofFunctionality() {
+  $("#poof").on("mouseenter", function () { $(this).remove() })
+}
+
+function addBlogFunctionality() {
+  $("#post-blog").click(function () {
+    const newEl = $("<div class='blog'>New blog</div>")
+    $("#blogs").append(newEl)
+
+    // newEl.on("mouseenter", function () {
+    //   $(this).text("blargh")
+    //   console.log("text")
+    // })
+  })
+
+  $("#blogs").on("mouseenter", ".blog", function () {
+    $(this).text("blargh")
+    console.log("text")
+  })
+}
+
 getHeader()
 setCSSStyles()
 setCSSStylesViaClass()
 $("#set-button").click(setInputValue)
+logBarcodeData()
+setHoverEventListener()
+setClickEventListener()
+setBoxHoverEventListener()
+setFeedMeFunctionality()
+addElementsInLoop()
+setPoofFunctionality()
+addBlogFunctionality()
